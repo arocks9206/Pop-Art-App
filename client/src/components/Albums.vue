caffd3ee0bca611e836611949382346908316164
Evgeny Nazarovs <eugene.nazarovs@gmail.com>
2020-05-07 12:09:51 +0100

    adds basic albums components

---
+++ client/src/components/Albums.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div>
+    <AlbumsList></AlbumsList>
+
+  </div>
+
+</template>
+
+<script>
+
+import AlbumsList from '@/components/AlbumsList.vue'
+
+export default {
+  name: 'Albums',
+  components: {
+    AlbumsList
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ client/src/components/AlbumsListItem.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'AlbumsListItem',
+  props: ['album'],
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
