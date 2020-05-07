2f6aab702d97700d3b2b0a10bbe143d6f6826d01
Evgeny Nazarovs <eugene.nazarovs@gmail.com>
2020-05-07 16:40:01 +0100

    adds basic designer components

---
+++ client/src/components/Designers/DesignersListItem.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div>
+
+
+
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'DesignerListItem',
+  props: ['designer']
+  // methods: {
+  //
+  // }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
