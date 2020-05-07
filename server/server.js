b74d281d0ff2c7357aabcad430df30cd1b24b650
Evgeny Nazarovs <eugene.nazarovs@gmail.com>
2020-05-07 14:39:40 +0100

    router changes

--- server/server.js
+++ server/server.js
@@ -10,17 +10,17 @@ app.use(cors())
 
 MongoClient.connect('mongodb://localhost:27017')
 .then((client) => {
-  const db = client.db('art_hub');
-  const artsCollection = db.collection('arts');
-  const artsRouter = createRouter(artsCollection);
-  app.use('/api/artist', artsRouter);
+  const db = client.db('popArt_hub');
+  const artistsCollection = db.collection('artists');
+  const artistsRouter = createRouter(artistsCollection);
+  app.use('/api/artists', artistsRouter);
 
 })
 .catch(console.error);
 
 MongoClient.connect('mongodb://localhost:27017')
 .then((client) => {
-  const db = client.db('film_hub');
+  const db = client.db('films_hub');
   const filmCollection = db.collection('film');
   const filmRouter = createRouter(filmCollection);
   app.use('/api/film', filmRouter);
