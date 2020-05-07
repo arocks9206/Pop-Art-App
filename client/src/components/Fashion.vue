cd6999206ae52a248ae9bd6b4740093511fda82b
Evgeny Nazarovs <eugene.nazarovs@gmail.com>
2020-05-07 11:55:58 +0100

    adds basic fashion components

---
+++ client/src/components/Fashion.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <div>
+    <FashionList></FashionList>
+
+  </div>
+
+</template>
+
+<script>
+
+import FashionList from '@/components/FashionList.vue'
+
+export default {
+  name: 'Fashion',
+  data(){
+    return {
+
+    }
+  },
+  components: {
+    FashionList
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ client/src/components/FashionListItem.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'FashionListItem',
+  props: ['designer'],
+  methods: {
+    
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
