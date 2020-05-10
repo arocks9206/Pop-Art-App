96d83b8c8db19be2349317130bd8a220d33d6044
Evgeny Nazarovs <eugene.nazarovs@gmail.com>
2020-05-10 20:14:28 +0100

    changes router

--- server/helpers/createRouter.js
+++ server/helpers/createRouter.js
@@ -20,7 +20,7 @@ const createRouter = function (collection) {
   router.get('/:id', (req, res) => {
     const id = req.params.id;
     collection
-    .findOne({ _id: id })
+    .findOne({ _id: { $in: [ id, ObjectID(id)]}})
     .then((doc) => res.json(doc))
     .catch((err) => {
       console.error(err);
