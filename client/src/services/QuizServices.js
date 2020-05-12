18adaac9ceb95db69b11a965955cdab4a6ad4b21
Evgeny Nazarovs <eugene.nazarovs@gmail.com>
2020-05-12 16:58:46 +0100

    fixes quiz

--- client/src/services/QuizServices.js
+++ client/src/services/QuizServices.js
@@ -1,21 +1,38 @@
 const baseURL = "http://localhost:3000/api/quiz/"
-const userScoreID = '5eb92ca460aec65528f758a0'
 
 export default {
-  getQuestions(id){
-    return fetch(baseURL + id)
+  getQuizData(){
+    return fetch(baseURL)
     .then(res => res.json())
   },
-  getUserScore(){
-    return fetch(baseURL + userScoreID)
-    .then(res => res.json())
-  },
-  updateUserScore(payload){
-    return fetch(baseURL + userScoreID, {
+  updateUserScore(id){
+    return fetch(baseURL + id, {
       method: 'PUT',
-      body: JSON.stringify(payload),
+      body: JSON.stringify,
       headers: {'Content-Type': 'application/json'}
     })
     .then(res => res.json())
   }
 }
+
+
+
+
+//
+//   getQuestions(id){
+//     return fetch(baseURL + id)
+//     .then(res => res.json())
+//   },
+//   getUserScore(){
+//     return fetch(baseURL + userScoreID)
+//     .then(res => res.json())
+//   },
+//   updateUserScore(payload){
+//     return fetch(baseURL + userScoreID, {
+//       method: 'PUT',
+//       body: JSON.stringify(payload),
+//       headers: {'Content-Type': 'application/json'}
+//     })
+//     .then(res => res.json())
+//   }
+// }
